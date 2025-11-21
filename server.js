1c08029058429532d9d0cfd61b400cd230c54c74
Provash-roy <provashray94@gmail.com>
2025-11-21 23:22:32 +0600

    First Commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const mongoose = require("mongoose");
+const Product = require("./Models/Product");
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB successfully");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.get("/products", async (req, res) => {
+  try {
+    const product = await Product.find();
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/products", async (req, res) => {
+  try {
+    const { name, price, description } = req.body;
+    const newProduct = new Product({ name, price, description });
+    const savedProduct = await newProduct.save();
+    res.json(savedProduct);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
